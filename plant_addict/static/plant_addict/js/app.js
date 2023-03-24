432c0a04b651222216f486b7ea914fc6be4fec1c
Sébastien <sebastien.mariaux@protonmail.com>
2023-03-24 11:19:12 +0100

    Style image field

--- plant_addict/static/plant_addict/js/app.js
+++ plant_addict/static/plant_addict/js/app.js
@@ -1,8 +1,9 @@
 import { Application } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
 import messagesController from "./messages_controller.js"
 import ajaxSelectController from "./ajax_select_controller.js"
+import fileSelectController from "./file_select_controller.js"
 
 window.Stimulus = Application.start()
 Stimulus.register("messages", messagesController)
 Stimulus.register("ajax-select", ajaxSelectController)
-
+Stimulus.register("file-select", fileSelectController)
---
+++ plant_addict/static/plant_addict/js/file_select_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+
+
+export default class FileSelectController extends Controller {
+  static targets = ["display"]
+
+  loadImage(event) {
+    this.displayTarget.src = URL.createObjectURL(event.target.files[0])
+  }
+
+}
